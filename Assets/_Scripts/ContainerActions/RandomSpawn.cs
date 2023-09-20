270655cb3501879fb1692502cba667b720cb81de
ALFARD777 <alfard6066@gmail.com>
2023-09-21 00:07:05 +0300

    beta 1.1.4

---
+++ Assets/_Scripts/ContainerActions/RandomSpawn.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    [HideInInspector] public List<GameObject> cars;
+    public List<GameObject> sovietCars;
+    public List<GameObject> americaCars;
+    public List<GameObject> dubaiCars;
+    public List<GameObject> japanCars;
+    public List<GameObject> euroCars;
+    public List<Transform> spawnPoints;
+    public RunOpen RunOpen;
+    void Start()
+    {
+        spawnPoints = new List<Transform>(spawnPoints);
+        sovietCars = new List<GameObject>(sovietCars);
+        americaCars = new List<GameObject>(americaCars);
+        dubaiCars = new List<GameObject>(dubaiCars);
+        japanCars = new List<GameObject>(japanCars);
+        euroCars = new List<GameObject>(euroCars);
+    }
+    public void SpawnCars()
+    {
+        switch (RunOpen.containerIntCost / 1000000)
+        {
+            case 1:
+                cars = sovietCars;
+                break;
+            case 3:
+                cars = americaCars;
+                break;
+            case 12:
+                cars = dubaiCars;
+                break;
+            case 7:
+                cars = japanCars;
+                break;
+            case 5:
+                cars = euroCars;
+                break;
+            default:
+                Debug.LogError("Неверный switch. Открываем EURO");
+                cars = euroCars;
+                break;
+        }
+        int car = UnityEngine.Random.Range(0, cars.Count);
+        GameObject go = Instantiate(cars[car], spawnPoints[RunOpen.containerToOpen].transform.position, Quaternion.identity);
+        Destroy(go.GetComponent(typeof(Rigidbody)));
+        Destroy(go.GetComponent(typeof(BoxCollider)));
+        Destroy(go.GetComponent(typeof(MeshCollider)));
+        Destroy(go.GetComponent(typeof(SphereCollider)));
+    }
+}
